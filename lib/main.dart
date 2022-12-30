ba5f58fd62e1ebc90a90f0bd33ad2c59be058a1d
Shailja <shailjachauhan.1036@gmail.com>
2022-12-30 11:22:48 +0530

    home page commented

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Visitor Application'),
+      //home changed
+
+      home: const MyHomePage(title: 'Visitor Application'),
     );
   }
 }
